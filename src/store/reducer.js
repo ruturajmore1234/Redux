fc6edcf1b21564e81e1375704edfb419fe82706d
Ruturaj More <ruturajmore1234@gmail.com>
2020-07-02 00:36:46 +0530

    master: 263. Switch-Case in the Reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,24 +3,25 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  if(action.type === 'INCREMENT'){
-    return {
-      counter: state.counter + 1
-    }
-  }else if(action.type === 'DECREMENT'){
-    return {
-      counter: state.counter - 1
-    }
-  }else if(action.type === 'ADD'){
-    return {
-      counter: state.counter + action.value
-    }
-  }else if(action.type === 'SUBSTRACT'){
-    return {
-      counter: state.counter - action.value
-    }
-  }else{
-    return state;
+  switch (action.type){
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1
+      }
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1
+      }
+    case 'ADD':
+      return {
+        counter: state.counter + action.value
+      }
+    case 'SUBSTRACT':
+      return {
+        counter: state.counter - action.value
+      }
+    default:
+      return state;
   }
 };
 
